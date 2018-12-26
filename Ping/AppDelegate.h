05c2bdddf53068cda05ee0f4e9c0ec7e7a05fd7f
飞鱼 <870027381@qq.com>
2018-12-26 11:05:27 +0800

    Initial Commit

---
+++ Ping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ping
+//
+//  Created by 飞鱼 on 2018/12/26.
+//  Copyright © 2018 RongZhiXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ping/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Ping
+//
+//  Created by 飞鱼 on 2018/12/26.
+//  Copyright © 2018 RongZhiXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
